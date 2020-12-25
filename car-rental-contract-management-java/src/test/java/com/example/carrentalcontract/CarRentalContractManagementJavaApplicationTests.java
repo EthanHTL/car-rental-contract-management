e4f483874374a9922542d4646ea904fedf5c756b
huangtianliang <2388092952@qq.com>
2020-12-25 16:59:05 +0800

    init

---
+++ car-rental-contract-management-java/src/test/java/com/example/carrentalcontract/CarRentalContractManagementJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrentalcontract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalContractManagementJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
