62d53cc7190803b0344ca521280fa27de1eee498
RyanHTL <2388092952@qq.com>
2021-01-23 23:51:27 +0800

    新增：activiti:进阶整合springboot

--- src/main/java/com/example/carrentalcontract/CarRentalContractManagementJavaApplication.java
+++ src/main/java/com/example/carrentalcontract/CarRentalContractManagementJavaApplication.java
@@ -3,11 +3,13 @@ package com.example.carrentalcontract;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import tk.mybatis.spring.annotation.MapperScan;
 
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.example.carrentalcontract.mapper")
+@PropertySource("classpath:application.yml")
 public class CarRentalContractManagementJavaApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/carrentalcontract/config/DemoApplicationConfig.java
+++ src/main/java/com/example/carrentalcontract/config/DemoApplicationConfig.java
@@ -1,4 +1,4 @@
-package com.example.carrentalcontract;
+package com.example.carrentalcontract.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
