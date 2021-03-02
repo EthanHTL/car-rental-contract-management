a0364cf26ef3bcd910eb8a8fc83190bb668e102d
huangtianliang <2388092952@qq.com>
2021-03-02 18:00:03 +0800

    修复：打包版本

--- src/main/java/com/example/carrentalcontract/aop/NotNullAop.java
+++ src/main/java/com/example/carrentalcontract/aop/NotNullAop.java
@@ -2,23 +2,19 @@ package com.example.carrentalcontract.aop;
 
 import com.example.carrentalcontract.annotation.NotNull;
 import com.example.carrentalcontract.exception.NotNullException;
-import com.sun.deploy.panel.ITreeNode;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.aop.aspectj.MethodInvocationProceedingJoinPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
-import java.util.Objects;
-import java.util.Optional;
+
 
 /**
  * @ClassName MethodAop
